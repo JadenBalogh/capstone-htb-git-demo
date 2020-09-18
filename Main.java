1e320950424514c8d17fad5e845d2cf68cba485f
Jaden Balogh <jadenbalogh@gmail.com>
2020-09-18 13:05:50 -0700

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4 };
+    System.out.println("Sum: " + getSum(arr));
+    System.out.println("Mean: " + getSum(arr));
+    System.out.println("Median: " + getSum(arr));
+    System.out.println("Mode: " + getSum(arr));
+  }
+
+  // Assigned to Athena
+  public static int getSum(int[] arr) {
+    return 0;
+  }
+
+  // Assigned to Jared
+  public static float getMean(int[] arr) {
+    return 0;
+  }
+
+  // Assigned to Alvin
+  public static float getMedian(int[] arr) {
+    return 0;
+  }
+
+  // Assigned to Jaden
+  public static int getMode(int[] arr) {
+    return 0;
+  }
+}

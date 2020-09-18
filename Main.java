61c423a2cb5c96c940ce7991e27ec213cbee4e14
Jaden Balogh <jadenbalogh@gmail.com>
2020-09-18 16:58:54 -0700

    Start first pass at getMode

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
     int[] arr = { 1, 2, 3, 4 };
@@ -24,6 +26,25 @@ public class Main {
 
   // Assigned to Jaden
   public static int getMode(int[] arr) {
+    // // Store the number of occurrences of each integer
+    // ArrayList<> counts = new ArrayList<Integer>();
+    // for (int i = 0; i < arr.length; i++) {
+    //   int idx = arr[i];
+    //   counts.set(idx, counts.get(idx) + 1);
+    // }
+    
+    // // Find the max number of occurrences (return the first in case of tie)
+    // int maxValue = arr[0];
+    // int maxIdx = 0;
+    // for (int i = 1; i < counts.size(); i++) {
+    //   int count = counts.get(i);
+    //   if (count > maxValue) {
+    //     maxValue = count;
+    //     maxIdx = i;
+    //   }
+    // }
+
+    // Return the integer with max occurrences
     return 0;
   }
 }

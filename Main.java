2ef7a25f2b6761bd5f93067af73cdf257e438d0f
Jaden Balogh <jadenbalogh@gmail.com>
2020-09-18 20:45:43 -0700

    Update workplace settings and mode function

--- Main.java
+++ Main.java
@@ -1,50 +1,54 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4 };
-    System.out.println("Sum: " + getSum(arr));
-    System.out.println("Mean: " + getSum(arr));
-    System.out.println("Median: " + getSum(arr));
-    System.out.println("Mode: " + getSum(arr));
-  }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        System.out.println("Sum: " + getSum(arr));
+        System.out.println("Mean: " + getMean(arr));
+        System.out.println("Median: " + getMedian(arr));
+        System.out.println("Mode: " + getMode(arr));
+    }
 
-  // Assigned to Athena
-  public static int getSum(int[] arr) {
-    return 0;
-  }
+    // Assigned to Athena
+    public static int getSum(int[] arr) {
+        return 0;
+    }
 
-  // Assigned to Jared
-  public static float getMean(int[] arr) {
-    return 0;
-  }
+    // Assigned to Jared
+    public static float getMean(int[] arr) {
+        return 0;
+    }
 
-  // Assigned to Alvin
-  public static float getMedian(int[] arr) {
-    return 0;
-  }
+    // Assigned to Alvin
+    public static float getMedian(int[] arr) {
+        return 0;
+    }
 
-  // Assigned to Jaden
-  public static int getMode(int[] arr) {
-    // // Store the number of occurrences of each integer
-    // ArrayList<> counts = new ArrayList<Integer>();
-    // for (int i = 0; i < arr.length; i++) {
-    //   int idx = arr[i];
-    //   counts.set(idx, counts.get(idx) + 1);
-    // }
-    
-    // // Find the max number of occurrences (return the first in case of tie)
-    // int maxValue = arr[0];
-    // int maxIdx = 0;
-    // for (int i = 1; i < counts.size(); i++) {
-    //   int count = counts.get(i);
-    //   if (count > maxValue) {
-    //     maxValue = count;
-    //     maxIdx = i;
-    //   }
-    // }
+    // Assigned to Jaden
+    public static int getMode(int[] arr) {
+        // Make a sorted version of arr
+        int[] myArr = new int[arr.length];
+        System.arraycopy(arr, 0, myArr, 0, arr.length);
+        Arrays.sort(myArr);
+        
+        // Find the most frequently occurring integer (or the first in a tie)
+        int mode = myArr[0];
+        int maxCount = 0;
+        int currentVal = myArr[0];
+        int currentCount = 0;
+        for (int i = 0; i < myArr.length; i++) {
+            if (myArr[i] != currentVal) {
+                currentCount = 0;
+            }
+            currentVal = myArr[i];
+            currentCount++;
+            if (currentCount > maxCount) {
+                mode = myArr[i];
+                maxCount = currentCount;
+            }
+        }
 
-    // Return the integer with max occurrences
-    return 0;
-  }
+        // Return the mode
+        return mode;
+    }
 }

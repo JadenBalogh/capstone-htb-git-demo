4d8dde348fade2c87cdabd567dbf8ac416d741fd
AA666666 <52806201+AA666666@users.noreply.github.com>
2020-09-22 12:24:32 -0700

    2

--- Main.java
+++ Main.java
@@ -12,8 +12,9 @@ public class Main {
     // Assigned to Athena
     public static int getSum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++){
             sum=sum+arr[i];
+        }
             return sum;
     }
 
